0da81fd27859b8dbcb6ba1a0ffde3dbf070739eb
yschristian <yschristian>
2022-09-06 14:41:42 +0200

    products models

---
+++ src/models/Product.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const productSchema = new mongoose.Schema({
+    title : { type: String,required:true, unique: true },
+    desc : { type: String,required:true },
+    img : { type: String,required:true },
+    categories : { type: Array },
+    size : { type: String, },
+    color : { type: String, },
+    price : { type: Number,required:true },
+},{
+    timestamps:true
+}
+)
+
+const Product = mongoose.model("products", productSchema)
+export default Product
