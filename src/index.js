37787448da76839cabf6bb8518d5f4ef5350ac2d
yschristian <yschristian>
2022-09-06 21:03:21 +0200

    server side

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ import connectDb from "./database/dbConnect"
 import userRouter from "./routes/userRoute"
 import productRouter from "./routes/productRouter"
 import orderRouter from "./routes/orderRouter"
+import cartRouter from "./routes/cartRouter"
+import stripeRouter from "./routes/stripeRouter";
+import cors from "cors"
 
 dotenv.config()
 
-const app = express();
 
+const app = express();
+app.use(cors({origin: "*"}));
 const port = process.env.port;
 
 app.use(express.json());
 app.use("/api/user",userRouter);
 app.use("/api/product",productRouter);
 app.use("/api/order",orderRouter);
+app.use("/api/cart",cartRouter)
+app.use("/api/checkout",stripeRouter)
 
 
 
