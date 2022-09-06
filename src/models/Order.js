f3d9db652ca7a30be08bd54857edea9bbcfe81a2
yschristian <yschristian>
2022-09-06 14:42:19 +0200

    Ordes of system

---
+++ src/models/Order.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+const orderSchema = new mongoose.Schema({
+    userId :{type:String , required: true},
+    products:[{
+        productId:{
+            type: String
+        },
+        quantity:{
+            type:Number,
+            default:1
+        },
+    },
+    ],
+    amount:{type: Number,required: true },
+    address:{type: Object,required: true},
+    status:{type: String,default: "pending"}
+},{
+    timestamps:true
+})
+
+const Order = mongoose.model("orders", orderSchema)
+export default Order
