a4b24a1a548a8bd3b5999189a8d01c1f1dcd143a
yschristian <yubahwesc@gmail.com>
2022-09-08 16:11:52 +0200

    product verified

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
 import productController from "../controllers/product"
 import upload from "../helper/multer";
-import { verifyTokenAndAuthorization ,verifyToken} from "../middleware/verifyToken";
+import { verifyTokenAndAuthorization ,verifyToken,verifyTokenAndAdmin} from "../middleware/verifyToken";
 
 const productRouter = express.Router()
 
-productRouter.post("/create", upload.single("img"), productController.createProduct)
+productRouter.post("/create", verifyTokenAndAdmin,upload.single("img"), productController.createProduct)
 productRouter.get("/getAllProduct",productController.getAll)
-productRouter.put("/update/:_id",verifyTokenAndAuthorization,productController.updateProduct)
+productRouter.put("/update/:_id",verifyTokenAndAdmin,productController.updateProduct)
 productRouter.get("/getProduct/:_id",productController.getSingleProduct)
-productRouter.delete("/delete/:_id",productController.delete)
+productRouter.delete("/delete/:_id",verifyTokenAndAdmin,productController.delete)
 
 export default productRouter
