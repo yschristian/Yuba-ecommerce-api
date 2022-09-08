0fcc1248a48485ce94112196010b620cd629773d
yschristian <yschristian>
2022-09-08 15:57:17 +0200

    cart verified

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cartController from "../controllers/cart"
-
+import { verifyToken, verifyTokenAndAuthorization,verifyTokenAndAdmin } from "../middleware/verifyToken";
 const cartRouter = express.Router()
 
-cartRouter.post("/create",cartController.createCart)
-cartRouter.get("/getAllCart",cartController.getAll)
-cartRouter.get("/getCart/:userId",cartController.getSingleCart)
-cartRouter.put("/update/:_id",cartController.updateCart)
-cartRouter.delete("/delete/:_id",cartController.delete)
+cartRouter.post("/create",verifyToken,cartController.createCart)
+cartRouter.get("/getAllCart",verifyTokenAndAdmin,cartController.getAll)
+cartRouter.get("/getCart/:userId",verifyTokenAndAuthorization,cartController.getSingleCart)
+cartRouter.put("/update/:_id",verifyTokenAndAuthorization,cartController.updateCart)
+cartRouter.delete("/delete/:_id",verifyTokenAndAuthorization,cartController.delete)
 
 
 
