2e4e7b90fff702543c783fe415560623006f27b8
yschristian <yubahwesc@gmail.com>
2022-09-08 16:09:22 +0200

    order verified

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -1,13 +1,15 @@
 import express from "express";
 import orderController from "../controllers/order"
+import { verifyToken, verifyTokenAndAuthorization,verifyTokenAndAdmin } from "../middleware/verifyToken";
+
 
 const orderRouter = express.Router()
 
-orderRouter.post("/create",orderController.createOrder)
-orderRouter.get("/getAllOrder",orderController.getAll)
-orderRouter.get("/income",orderController.income)
-orderRouter.put("/update/:_id",orderController.updateOrder)
-orderRouter.get("/getOrder/:userId",orderController.getSingleOrder)
-orderRouter.delete("/delete/:_id",orderController.delete)
+orderRouter.post("/create",verifyToken,orderController.createOrder)
+orderRouter.get("/getAllOrder",verifyTokenAndAdmin,orderController.getAll)
+orderRouter.get("/income",verifyTokenAndAdmin,orderController.income)
+orderRouter.put("/update/:_id",verifyTokenAndAdmin,orderController.updateOrder)
+orderRouter.get("/getOrder/:userId",verifyTokenAndAuthorization,orderController.getSingleOrder)
+orderRouter.delete("/delete/:_id",verifyTokenAndAdmin,orderController.delete)
 
 export default orderRouter
