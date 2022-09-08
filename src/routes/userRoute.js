ea5277ff87cfe7cf806a229788d6fd64902edc23
yschristian <yubahwesc@gmail.com>
2022-09-08 16:13:01 +0200

    user verified

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,15 +1,15 @@
 import express from "express";
 import userController from "../controllers/user";
-import { verifyTokenAndAuthorization, verifyTokenAndAdmin } from "../middleware/verifyToken";
+import { verifyTokenAndAuthorization, verifyTokenAndAdmin,verifyToken } from "../middleware/verifyToken";
 
 const userRouter = express.Router()
 
 userRouter.post("/register" , userController.register)
 userRouter.post("/login",userController.login)
 userRouter.put("/update/:_id",verifyTokenAndAuthorization,userController.updateUser)
-userRouter.get("/getAllUser",userController.getAll)
-userRouter.get("/findUser/:_id",userController.getSingleUser)
-userRouter.delete("/delete/:_id",userController.delete)
-userRouter.get("/stats", userController.stats)
+userRouter.get("/getAllUser",verifyTokenAndAdmin,userController.getAll)
+userRouter.get("/findUser/:_id",verifyTokenAndAdmin,userController.getSingleUser)
+userRouter.delete("/delete/:_id",verifyTokenAndAuthorization,userController.delete)
+userRouter.get("/stats",verifyTokenAndAdmin, userController.stats)
 
 export default userRouter
