ca7ac76033d6cb55a6ed362b6a3f7c378a6ccb69
yschristian <yubahwesc@gmail.com>
2022-09-15 16:56:40 +0200

    new feature

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -59,12 +59,16 @@ class orderController{
 
     // GET MONTHLY INCOME
     static async income(req,res){
+        const productId = req.query.pid
         const date = new Date()
         const lastMonth = new Date(date.setMonth(date.getMonth() - 1))
         const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1))
         try {
             const income = await Order.aggregate([
-                { $match: {createdAt : {$gte : previousMonth }}},
+                { $match: {createdAt : {$gte : previousMonth }, 
+                ...(productId && {
+                    products:{$elemMatch: {productId}}
+                })}},
                 {
                     $project:{
                         month:{ $month:"$createdAt" },
